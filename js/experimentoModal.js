57eea7782f7e2f47acc84965b969a09642c01864
David Gomez <lastfors51@gmail.com>
2024-08-09 14:19:54 -0500

    PrimerComment

---
+++ js/experimentoModal.js
@@ -0,0 +1,9 @@
+const btnAbrirModal =
+document.querySelector(".btn-abrir-modal");
+const btnCerrarModal =
+document.querySelector(".btn-cerrar-modal");
+const DavidModal =
+document.querySelector(".modal-david");
+
+btnAbrirModal("click",()=>{DavidModal.showModal()
+})
